110681b6ea93aca60c7c1b753a0613adaea1b94b
Abdulrahman Ibrahim <58425053+Ibra-sudo@users.noreply.github.com>
2024-07-10 16:37:51 +0200

    some work by Feed View

---
+++ threads-clone/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCell.swift
+//  threads-clone
+//
+//  Created by Abdulrahman Ibrahim on 10.07.24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
